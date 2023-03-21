d8ab2cc5e7101381a6a7a7eefc807f743f0726ca
ridwan <ridwanediallo99@gmail.com>
2023-03-21 08:05:50 +0000

    Add registration controller to devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'registrations'
+  }
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
