4e1fd89943f41e48c8c6b12ec568fbb9ac70f3c4
ridwan <ridwanediallo99@gmail.com>
2023-03-21 07:59:08 +0000

    Add hotwire to dismiss notice.

---
+++ app/javascript/controllers/flash_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    setTimeout(() => {
+      this.dismiss();
+    }, 5000);
+  }
+
+  dismiss() {
+    this.element.remove();
+  }
+
+}
