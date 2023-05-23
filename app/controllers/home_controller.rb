0a1447fabf3af1f528de8afb50a3b59e3f725124
ridwan <ridwanediallo99@gmail.com>
2023-05-23 18:38:28 +0000

    Add home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
-  def index
+ def index
+     @page = params[:page] || 1
+     conn = Faraday.new(url: 'https://gutendex.com')
+     response = conn.get("/books/?page=#{@page}")
+     result = JSON.parse(response.body)
+     @books = result['results']
+     @next_page = result['next']
+     @previous_page = result['previous']
   end
 end
