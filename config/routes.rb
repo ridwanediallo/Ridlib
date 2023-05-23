79198dd12718b7f3817bd40bef3cfeb5027290e2
ridwan <ridwanediallo99@gmail.com>
2023-05-23 18:47:47 +0000

    Clean routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,13 @@
 Rails.application.routes.draw do
-  # resources :authors
-  # get 'books/index'
+
   resources :authors do
     resources :books
   end
-  # get 'get_books' to: 'books#get_books'
-  # resources :books
+
   devise_for :users, controllers: {
     registrations: 'registrations'
   }
+
   root 'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
-
-  def get_books
-    url = 'https://gutendex.com/books'
-    response = RestClient.get(url)
-    render json: response
-  end
