aaa686923ea6e27cf8536c1a59a82f561c64eab5
ridwan <ridwanediallo99@gmail.com>
2023-08-06 23:24:00 +0000

    Update comments query to fix N+1 problem

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   before_action :set_comment, only: %i[ show edit update destroy ]
 
   def index
-    @comments = Comment.all
+    @comments = Comment.includes(:book, :user).all
   end
 
 
