90b43d3d388b2b911f98b7c59673dffd1d824c98
ridwan <ridwanediallo99@gmail.com>
2023-09-02 18:30:56 +0000

    Add rating model and ralation between user and book

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,5 @@ class Book < ApplicationRecord
     # belongs_to :author
     has_many :comments, dependent: :destroy
     has_many :likes, dependent: :destroy
+    has_many :rates, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
 
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :ratings, dependent: :destroy
 end
