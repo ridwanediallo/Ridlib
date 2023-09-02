073655d6a7afdc037f9417cf09292d879bc26801
ridwan <ridwanediallo99@gmail.com>
2023-09-02 18:31:52 +0000

    Add routes for rating

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,13 @@ Rails.application.routes.draw do
   resources :books do
     member do
       post :create_comment
-
+      # post 'ratings', to: 'books#rate'
     end
     resources :comments, only: [:create]
   end
 
   resources :likes, only: [:create, :destroy]
+  resources :rates, only: [:create]
 
 
 
