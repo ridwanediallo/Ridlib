78505b6540cb9f6618a4b79004ce6f24aeec9cb0
ridwan <ridwanediallo99@gmail.com>
2023-09-12 16:53:30 +0000

    U[date home controller and add lazy_load method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,24 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!, only: [:index]
-  
- def index
+
+  def index
+     @fetched_books = fetch_books
+  end
+
+
+  def lazy_load
+    @fetched_books = fetch_books
+     render partial: "books/books", books: @fetched_books
+  end
+
+  private
+
+  def fetch_books
      @page = params[:page] || 1
      conn = Faraday.new(url: 'https://gutendex.com')
      response = conn.get("/books/?page=#{@page}")
      result = JSON.parse(response.body)
-     @fetched_books = result['results']
-     @next_page = result['next']
-     @previous_page = result['previous']
+     fetched_books = result['results']
   end
 
 end
