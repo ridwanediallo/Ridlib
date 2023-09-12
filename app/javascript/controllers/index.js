db87f56fe298a1d53a66c69b501293a23187a0a4
ridwan <ridwanediallo99@gmail.com>
2023-09-12 16:51:00 +0000

    Set up stimulus-content-loader

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,8 @@
 // Import and register all your controllers from the importmap under controllers/*
 
 import { application } from "./application"
+import ContentLoader from 'stimulus-content-loader';
 
 import FlashCcontroller from "./flash_controller"
 application.register('flash', FlashCcontroller)
+application.register('content-loader', ContentLoader);
