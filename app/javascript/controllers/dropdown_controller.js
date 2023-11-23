fa0fb3a38659ae39e4d33d2e6a1fa7565f7c5afd
ridwan <ridwanediallo99@gmail.com>
2023-11-23 11:15:50 +0000

    Add dropdown stimulus controller

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -3,7 +3,25 @@ import { Controller } from '@hotwired/stimulus';
 export default class extends Controller {
   static targets = ['menu', 'dropdown'];
 
+  connect() {
+    // Add a click event listener to the document body
+    document.body.addEventListener('click', this.closeDropdown.bind(this));
+  }
+
+  disconnect() {
+    // Remove the click event listener when the controller is disconnected
+    document.body.removeEventListener('click', this.closeDropdown.bind(this));
+  }
+
   toggleDropdown() {
     this.dropdownTarget.classList.toggle('hidden');
+  }
+
+  closeDropdown(event) {
+    // Check if the click event target is outside the dropdown menu
+    if (!this.menuTarget.contains(event.target)) {
+      // Close the dropdown menu
+      this.dropdownTarget.classList.add('hidden');
+    }
   }
 }
