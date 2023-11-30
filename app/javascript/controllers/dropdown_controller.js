ef12e456b8e2a44b929b1a586853c2e269439a93
ridwan <ridwanediallo99@gmail.com>
2023-11-30 21:26:55 +0000

    Remove commented code

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -4,12 +4,10 @@ export default class extends Controller {
   static targets = ['menu', 'dropdown'];
 
   connect() {
-    // Add a click event listener to the document body
     document.body.addEventListener('click', this.closeDropdown.bind(this));
   }
 
   disconnect() {
-    // Remove the click event listener when the controller is disconnected
     document.body.removeEventListener('click', this.closeDropdown.bind(this));
   }
 
@@ -18,9 +16,7 @@ export default class extends Controller {
   }
 
   closeDropdown(event) {
-    // Check if the click event target is outside the dropdown menu
     if (!this.menuTarget.contains(event.target)) {
-      // Close the dropdown menu
       this.dropdownTarget.classList.add('hidden');
     }
   }
