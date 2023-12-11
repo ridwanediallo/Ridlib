d57afd14d1db91955b9d1e5f95829fbe1fdff23b
ridwan <ridwanediallo99@gmail.com>
2023-12-11 22:31:27 +0000

    Validate rating

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,6 @@
 class Rating < ApplicationRecord
   belongs_to :user
   belongs_to :book
+
+  validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
