909e8ef0ea6f0582f3c2d5c4c3b753b678d0f702
ridwan <ridwanediallo99@gmail.com>
2023-12-19 18:21:55 +0000

    Add back create_comment route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
 
   resources :books do
+    member do
+      post :create_comment
+      # post 'ratings', to: 'books#rate'
+    end
     resources :comments
   end
 
   resources :likes, only: [:create, :destroy]
   resources :rating, only: [:create]
+
+
 
   devise_for :users, controllers: {
     registrations: 'registrations'
