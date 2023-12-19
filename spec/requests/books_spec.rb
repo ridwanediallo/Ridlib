f7983a03cce35afb3081ca25215d9a8bf7208da9
ridwan <ridwanediallo99@gmail.com>
2023-12-19 18:19:44 +0000

    Update rspec for book_path request

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -2,13 +2,12 @@ require 'rails_helper'
 
 
 RSpec.describe 'Books', type: :request do
-  describe "Request list of all books" do
-    let!(:book) { Book.create(title: 'Example Book') }
+  describe "Get #show" do
+    let!(:book) { Book.create(book_id: 1, title: 'Pride and Prejudice', author_name: 'Austen, Jane', subject: 'Courtship -- Fiction', image_url: 'https://www.gutenberg.org/cache/epub/1342/pg1342.cover.medium.jpg', download_count: 53375) }
 
     it 'returns a successful response' do
-      get books_path
+      get book_path(book.id)
       expect(response).to be_successful
-      expect(response.body).to include(book.title)
     end
   end
 end
