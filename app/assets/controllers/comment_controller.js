4eafb3a19e6130f11e78577bc53ed11f70e68043
ridwan <ridwanediallo99@gmail.com>
2023-12-22 22:05:22 +0000

    Remove controllers folder from assest after adding it for test

--- app/assets/controllers/comment_controller.js
+++
@@ -1,8 +0,0 @@
-import { Controller } from '@hotwired/stimulus';
-
-export default class extends Controller {
-
-  dismiss() {
-    this.element.remove();
-  }
-}
--- app/assets/controllers/index.js
+++
@@ -1,9 +0,0 @@
-// Import and register all your controllers from the importmap under controllers/*
-
-import { application } from "./application"
-
-import FlashCcontroller from "./flash_controller"
-application.register('flash', FlashCcontroller)
-
-import DropdownCcontroller from "./dropdown_controller";
-application.register('dropdown', DropdownCcontroller)
