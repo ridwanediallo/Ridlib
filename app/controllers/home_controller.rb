3ef2d115066c625e0006df9e7d302b00c305ca0e
ridwan <ridwanediallo99@gmail.com>
2024-01-04 21:41:54 +0000

    Update home controller to allow browsing by topic

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,17 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!, only: [:index]
 
- def index
-     @page = params[:page] || 1
-     conn = Faraday.new(url: 'https://gutendex.com')
-     response = conn.get("/books/?page=#{@page}")
-     result = JSON.parse(response.body)
-     @fetched_books = result['results']
-     @next_page = result['next']
-     @previous_page = result['previous']
-  end
+def index
+    @page = params[:page] || 1
+    @topic = params[:topic]
+
+    conn = Faraday.new(url: 'https://gutendex.com')
+    response = conn.get("/books/?page=#{@page}&topic=#{@topic}")
+    result = JSON.parse(response.body)
+    @fetched_books = result['results']
+    @next_page = result['next']
+    @previous_page = result['previous']
+end
 
 
   def search
@@ -32,4 +34,13 @@ class HomeController < ApplicationController
     render :index
   end
 
+
+  private
+
+  def fetch_books_by_topic(topic)
+    conn = Faraday.new(url: 'https://gutendex.com')
+    response = conn.get("/books/?topic=#{topic}")
+    result = JSON.parse(response.body)
+    result['results']
+  end
 end
